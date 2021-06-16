e1aebe8f0e805dd8dfebfa70b04cf6b88580bb88
Rafael <rafadellaquila@gmail.com>
2021-06-15 22:33:58 -0300

    requests API and get IBGE numbers ok

--- web/forms/diarista_form.py
+++ web/forms/diarista_form.py
@@ -1,5 +1,7 @@
+import json
 from django import forms
 from ..models import Diarista
+from ..services import cep_service
 
 
 class DiaristaForm(forms.ModelForm):
@@ -7,9 +9,11 @@ class DiaristaForm(forms.ModelForm):
     cep = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '00.000-000'}))
     telefone = forms.CharField(widget=forms.TextInput(attrs={'data-mask': '(00)00000-0000'}))
 
+    # codigo_ibge = forms.IntegerField(required=False)
+
     class Meta:
         model = Diarista
-        fields = '__all__'
+        exclude = ('codigo_ibge',)
 
     def clean_cpf(self):
         cpf = self.cleaned_data['cpf']
@@ -17,8 +21,23 @@ class DiaristaForm(forms.ModelForm):
 
     def clean_cep(self):
         cep = self.cleaned_data['cep']
+        cep_formatado = cep.replace(".", "").replace("-", "")
+        response = cep_service.buscar_cidade_cep(cep_formatado)
+        if response.status_code == 400:
+            raise forms.ValidationError("O CEP Informado está incorreto")
+        cidade_api = json.loads(response.content)
+        if 'erro' in cidade_api:
+            raise forms.ValidationError("O CEP informado não foi encontrado")
         return cep.replace(".", "").replace("-", "")
 
     def clean_telefone(self):
         telefone = self.cleaned_data['telefone']
         return telefone.replace("(", "").replace(")", "").replace("-", "")
+
+    def save(self, commit=True):
+        instance = super(DiaristaForm, self).save(commit=False)
+        response = cep_service.buscar_cidade_cep(self.cleaned_data.get('cep'))
+        cidade_api = json.loads(response.content)
+        instance.codigo_ibge = cidade_api['ibge']
+        instance.save()
+        return instance
